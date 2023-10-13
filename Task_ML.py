63151e145e6e80969fbef798e7cdc4c479e1ca92
Daan Michielsen <90918416+DaanMichielsen@users.noreply.github.com>
2023-10-13 14:15:47 +0200

    Update Task_ML.py

--- Task_ML.py
+++ Task_ML.py
@@ -31,7 +31,7 @@ def encode_data(df):
 def embed_pdf(pdf_path):
     with open(pdf_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 pdf_path = "ML_Benchmark_Daan_Michielsen.pdf"
